255eb74eb42e1a936b1333c99a318f374240fa99
Brandon Ros <brandonros1@gmail.com>
2020-10-14 23:47:37 -0400

    sboot into memory, no ELF

--- include/hw/tricore/tricore.h
+++ include/hw/tricore/tricore.h
@@ -7,4 +7,11 @@ struct tricore_boot_info {
     uint64_t ram_size;
     const char *kernel_filename;
 };
+
+struct memory_region {
+  const char *name;
+  uint32_t start;
+  uint32_t end;
+  uint32_t size;
+};
 #endif
